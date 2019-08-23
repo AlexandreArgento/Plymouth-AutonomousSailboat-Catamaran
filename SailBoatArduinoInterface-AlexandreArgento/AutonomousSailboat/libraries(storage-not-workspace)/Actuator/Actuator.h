b8a963819405fd6da2441c89c1af65fc8ad2965c
AlexandreArgento <38204042+AlexandreArgento@users.noreply.github.com>
2019-08-23 14:33:32 +0100

    Latest changes

--- SailBoatArduinoInterface-AlexandreArgento/AutonomousSailboat/libraries(storage-not-workspace)/Actuator/Actuator.h
+++ SailBoatArduinoInterface-AlexandreArgento/AutonomousSailboat/libraries(storage-not-workspace)/Actuator/Actuator.h
@@ -4,14 +4,14 @@
 #include <config-Sailboat.h>
 #include <Arduino.h>
 #include <Helper.h>
-#include <Log.h>
 
 #include <ros.h>
 
 
-class Actuator{
+class Actuator
+{
 	public:
-		Actuator(){}
+		Actuator() {}
 		
 		virtual void init();
 		virtual void applyCommand(double command) = 0;
@@ -20,11 +20,12 @@ class Actuator{
 };
 
 
-class ActuatorROS : public Actuator{
+class ActuatorROS : public Actuator
+{
 	public:
 		ActuatorROS(const char* name, ros::Msg* msg) : pub(name, msg), nh(NULL){}
-		void init(){}
-		virtual void init(ros::NodeHandle* n){n->advertise(pub); nh = n;}
+		void init() {}
+		virtual void init(ros::NodeHandle* n) {n->advertise(pub); nh = n;}
 		virtual void communicateData() = 0;
 		
 	protected:
